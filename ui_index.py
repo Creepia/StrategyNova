2e541bfc3972a84fd21708c9f98e65d2c43dc39d
Creepia <rainbowcreepia@gmail.com>
2024-01-17 02:28:25 +0800

    updt for requirements installation and clear folder tab in index page

--- ui_index.py
+++ ui_index.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from st_pages import show_pages_from_config
 import os
+from shutil import rmtree
 
 for folder in ['source/default_set','applied_indicators/default_set','signals/default_set','testback/default_set','summary/default_set']:
     if not os.path.exists(folder):
@@ -14,10 +15,24 @@ show_pages_from_config()
 '**Find different pages on the sidebar \:D**'
 '---'
 
-current_view = st.selectbox('Stock Set', tuple(os.listdir('source')),placeholder='default_set')
-list_source = os.listdir(f'source/{current_view}')
-view_data = pd.DataFrame({
-    'stock_file':list_source,
-    'shape':[pd.read_csv(f'source/{current_view}/{file}').shape for file in list_source]
-                          })
-st.dataframe(view_data,use_container_width=True)
+tab_view_source,tab_clear_folder= st.tabs(['View Source', 'Clear Folder'])
+with tab_view_source:
+    current_view = st.selectbox('Stock Set', tuple(os.listdir('source')),placeholder='default_set')
+    list_source = os.listdir(f'source/{current_view}')
+    view_data = pd.DataFrame({
+        'stock_file':list_source,
+        'shape':[pd.read_csv(f'source/{current_view}/{file}').shape for file in list_source]
+                            })
+    st.dataframe(view_data,use_container_width=True)
+
+with tab_clear_folder:
+    exist_folders=set(['applied_indicators','signals','testback','source','summary']).intersection(set(os.listdir('./')))
+    st.write(set(['applied_indicators','signals','testback','source','summary']))
+    st.write(set(os.listdir('./')))
+    option = st.selectbox('Choose the folder you want to clear...',exist_folders)
+    clear_selected_folders_1 = st.button('CLEAR SELECTED FOLDERS?',type='secondary',key='clear_selected_folders_1')
+    if st.button('CLEAR SELECTED FOLDERS!',type='primary',key='clear_selected_folders_2',disabled=not clear_selected_folders_1):
+        # Avoid misdeletings
+        if(len(option)>3):
+            rmtree(option)
+            st.success(f"Successfully clear '{option}' folder")
