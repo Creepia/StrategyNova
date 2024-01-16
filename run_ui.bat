e489acde28fc75b315da541ebb9ff5db0ec0e407
Creepia <rainbowcreepia@gmail.com>
2024-01-16 23:18:46 +0800

    compatibility updt

--- run_ui.bat
+++ run_ui.bat
@@ -2,7 +2,7 @@
 rem 检测当前目录下是否有python310文件夹
 if exist python310 (
   rem 检测是否有各种包
-  .\python310\python.exe -c "import numpy,pandas,streamlit,talib,efinance" 2>nul
+  .\python310\python.exe -c "import numpy,pandas,streamlit,talib,efinance,st_pages" 2>nul
   rem 如果有任何一个缺少，就安装所有的包
   if errorlevel 1 (
     echo Some packages are missing, installing from requirements.txt
