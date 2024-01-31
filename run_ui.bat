51f8a866ed9a8d0b00614bceb6943dcb63bbf3ac
Caion <rainbowcreepia@gmail.com>
2024-01-31 15:18:59 +0800

    bat updt

--- run_ui.bat
+++ run_ui.bat
@@ -1,13 +1,9 @@
 @echo off
-rem 检测当前目录下是否有python310文件夹
 if exist python310 (
-    rem pip安装所有的包
     echo Checking the requirements
     .\python310\python.exe -m pip install -r requirements.txt
 ) else (
-  rem 如果没有python310文件夹，就提示用户
   echo python310 folder is not found in the current directory
 )
-rem 运行程序
 echo Running home.py ...
 .\python310\Scripts\streamlit.exe run home.py
