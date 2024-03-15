5e9482a42eef7052df651359791b1f23d54063a6
CharonTANG <tianmushang@tangtiandeMacBook-Air.local>
2024-03-15 23:55:03 +0800

    增加 ARBR  策略（R上所有单策略都转移过来了）

--- strategy_tools.py
+++ strategy_tools.py
@@ -166,7 +166,7 @@ class Expression:
 
 
 
-ALL_STRATEGIES = ['SMA', 'MACD','AROON', 'RSI', 'BOLLING','KDJ','DMI','ROC','SMI', 'WPR', 'SAR','CCI', 'OBV', 'PVT']
+ALL_STRATEGIES = ['SMA', 'MACD','AROON', 'RSI', 'BOLLING','KDJ','DMI','ROC','SMI', 'WPR', 'SAR','CCI', 'OBV', 'PVT', 'ARBR']
 """
 Strategies added here will be able to be selected
 """
@@ -270,8 +270,19 @@ def apply_strategy(strategy:str, df:pd.DataFrame)->Expression:
         exp = Expression('PVT > PVT_lag1 and 收盘 > close_lag1 | PVT < PVT_lag1 and 收盘 < close_lag1', df)
         # 觉得这个策略很差劲。。
 
+    elif strategy == 'ARBR':
+        # AR人气指标 计算公式（以日为单位举例） AR = [N天所有（High-Open）的和/ N天所有（Open—Low）的和] * 100
+        # 查看人气程度，衡量市场上多空双方力量对比变化
+        df['AR'] = (df['最高'].rolling(26).sum() - df['开盘'].rolling(26).sum()) / (
+                    df['开盘'].rolling(26).sum() - df['最低'].rolling(26).sum()) * 100
+        # BR人气指标 计算公式（以日为单位举例） BR(n)=∑（当日最高价－昨日收盘价）÷∑（昨日收盘价－当日最低价）×100
+        df['BR'] = (df['最高'].rolling(26).sum() - df['收盘'].shift(1).rolling(26).sum()) / (
+                    df['收盘'].shift(1).rolling(26).sum() - df['最低'][1:].rolling(26).sum()) * 100
+        # BR < AR & & (BR[i] < 100 | | AR[i] < 60)， 买入
+        # if (AR[i] > 150 ，卖出
+        # 不完整！ 还有一种 buy的情况还没有加进去
+        exp = Expression('BR < AR and BR < 100 | AR > 150', df)
 
-        
     return exp
 
 
